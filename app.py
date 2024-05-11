80d34b5d99199ac6faa5ef950c2af63a0a6ed2e6
srikeerthis <srikeerthisrinivasan@gmail.com>
2024-05-11 15:57:39 -0500

    feat: added a database and routes to store user details

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
-# Dummy data
-users = [
-    {"name": "Alice", "status": "In Office"},
-    {"name": "Bob", "status": "Remote"},
-    {"name": "Charlie", "status": "Unavailable"}
-]
+db= SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(50),nullable=False)
+    status = db.Column(db.String(50),nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.name}>'
 
 @app.route('/')
 def index():
+    users = User.query.all()
     return render_template('index.html', users=users)
 
 @app.route('/api/users')
